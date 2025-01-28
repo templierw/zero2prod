3d535493268b47c95764b0500988815c6759e615
William TEMPLIER <william.templier@se.com>
2025-01-28 16:49:08 +0100

    feat: subscribe route

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -18,10 +18,61 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
+#[tokio::test]
+async fn subscribe_with_valid_form_200() {
+    // arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // act
+    let body = "name=the%/20Wall&email=theWall%40tqc.ca";
+    let response = client
+        .post(&format!("{}/subscribe", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("failed to execute request");
+
+    // assert
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_invalid_form_400() {
+    // arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=the%20Wall", "missing the email"),
+        ("email=theWall%40tqc.ca", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalib_body, error_message) in test_cases {
+        //act
+        let response = client
+            .post(&format!("{}/subscribe", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalib_body)
+            .send()
+            .await
+            .expect("failed to execute request");
+
+        // assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
+}
+
 fn spawn_app() -> String {
     let lst = TcpListener::bind("127.0.0.1:0").expect("failed to bind");
     let port = lst.local_addr().unwrap().port();
-
 
     let server = zero2prod::run(lst).expect("Failed to bind address");
     let _ = tokio::spawn(server);
