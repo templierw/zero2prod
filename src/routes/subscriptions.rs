3bae99d4144658277c487f6f58bee67484e31b5d
William Templier <55127511+templierw@users.noreply.github.com>
2025-02-07 13:34:07 +0000

    refactor: project structure

---
+++ src/routes/subscriptions.rs
@@ -0,0 +1,12 @@
+use actix_web::{web, HttpResponse};
+use serde;
+
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    pub email: String,
+    pub name: String,
+}
+
+pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
