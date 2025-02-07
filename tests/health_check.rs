ceba0589b181436582550fb59d2d072a20fe27b5
William Templier <55127511+templierw@users.noreply.github.com>
2025-02-07 13:34:07 +0000

    refactor: project structure

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -74,7 +74,7 @@ fn spawn_app() -> String {
     let lst = TcpListener::bind("127.0.0.1:0").expect("failed to bind");
     let port = lst.local_addr().unwrap().port();
 
-    let server = zero2prod::run(lst).expect("Failed to bind address");
+    let server = zero2prod::startup::run(lst).expect("Failed to bind address");
     let _ = tokio::spawn(server);
 
     format!("http://127.0.0.1:{}", port)
