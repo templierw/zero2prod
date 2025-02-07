a3a9a8f113cc9d5e313e4796a012d129dedf4036
William Templier <55127511+templierw@users.noreply.github.com>
2025-02-07 17:05:55 +0000

    feat(subscribe): db configuration

---
+++ src/configuration.rs
@@ -0,0 +1,47 @@
+use config;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct Settings {
+    pub database: DataBaseSettings,
+    pub application_port: u16,
+}
+
+#[derive(Deserialize)]
+pub struct DataBaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
+
+impl DataBaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    config::Config::builder()
+        .add_source(config::File::with_name("configuration"))
+        .build()?
+        .try_deserialize()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_connection_string() {
+        let config = get_configuration().unwrap();
+        assert_eq!(
+            config.database.connection_string(),
+            "postgres://postgres:password@127.0.0.1:5432/newsletter"
+        )
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::net::TcpListener;
 
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let lst = TcpListener::bind("127.0.0.1:8000").expect("failed to bind");
+    let configuration = get_configuration().expect("failed to read configuration file");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    let lst = TcpListener::bind(address)?;
     run(lst)?.await
 }
