9d93832d5cceaedfec0462b93984853ca68cfb99
William Templier <55127511+templierw@users.noreply.github.com>
2025-02-08 21:06:17 +0000

    feat(telemetry): init logger

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::net::TcpListener;
 
+use env_logger::{self, Env};
 use sqlx::PgPool;
+
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let config = get_configuration().expect("failed to read configuration file");
 
     let connection_pool = PgPool::connect(&config.database.connection_string())
--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,18 @@
 use std::net::TcpListener;
 
-use actix_web::{dev::Server, web, App, HttpServer};
+use actix_web::dev::Server;
+use actix_web::web::Data;
+use actix_web::{web, App, HttpServer};
+use actix_web::middleware::Logger;
 use sqlx::PgPool;
 
 use crate::routes::{health_check, subscribe};
 
 pub fn run(listener: TcpListener, connection: PgPool) -> Result<Server, std::io::Error> {
-    let db_pool = web::Data::new(connection);
+    let db_pool = Data::new(connection);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscribe", web::post().to(subscribe))
             .app_data(db_pool.clone())
