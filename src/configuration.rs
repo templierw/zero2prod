40094669db13656a5d5d3edcbc80532b126b758d
William Templier <55127511+templierw@users.noreply.github.com>
2025-02-16 18:13:18 +0000

    feat(configuration): update db connect

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,6 +1,9 @@
 use config;
 use secrecy::{ExposeSecret, SecretBox};
 use serde::Deserialize;
+use serde_aux::field_attributes::deserialize_number_from_string;
+use sqlx::ConnectOptions;
+use sqlx::postgres::{PgConnectOptions, PgSslMode};
 
 #[derive(Deserialize)]
 pub struct Settings {
@@ -10,44 +13,45 @@ pub struct Settings {
 
 #[derive(Deserialize)]
 pub struct ApplicationSettings {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
-    pub host: String
+    pub host: String,
 }
 
 #[derive(Deserialize)]
 pub struct DataBaseSettings {
     pub username: String,
     pub password: SecretBox<String>,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
     pub database_name: String,
+    pub require_ssl: bool,
 }
 
 impl DataBaseSettings {
-    pub fn connection_string(&self) -> String {
-        format!(
-            "postgres://{}:{}@{}:{}/{}",
-            self.username,
-            self.password.expose_secret(),
-            self.host,
-            self.port,
-            self.database_name
-        )
+    pub fn with_db(&self) -> PgConnectOptions {
+        self.without_db()
+            .database(&self.database_name)
+            .log_statements(tracing::log::LevelFilter::Trace)
     }
 
-    pub fn connection_string_no_db(&self) -> String {
-        format!(
-            "postgres://{}:{}@{}:{}",
-            self.username,
-            self.password.expose_secret(),
-            self.host,
-            self.port
-        )
+    pub fn without_db(&self) -> PgConnectOptions {
+        PgConnectOptions::new()
+            .host(&self.host)
+            .username(&self.username)
+            .password(&self.password.expose_secret())
+            .port(self.port)
+            .ssl_mode(if self.require_ssl {
+                PgSslMode::Require
+            } else {
+                PgSslMode::Prefer
+            })
     }
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
-    let env : Environment = std::env::var("APP_ENVIRONMENT")
+    let env: Environment = std::env::var("APP_ENVIRONMENT")
         .unwrap_or("local".into())
         .try_into()
         .expect("failed to parse APP_ENVIRONMENT");
@@ -59,20 +63,21 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     config::Config::builder()
         .add_source(config::File::from(config_path.join("base")).required(true))
         .add_source(config::File::from(config_path.join(env.as_str())).required(true))
+        .add_source(config::Environment::with_prefix("app").separator("__"))
         .build()?
         .try_deserialize()
 }
 
 pub enum Environment {
     Local,
-    Production
+    Production,
 }
 
 impl Environment {
     pub fn as_str(&self) -> &'static str {
         match self {
             Environment::Local => "local",
-            Environment::Production => "production"
+            Environment::Production => "production",
         }
     }
 }
@@ -84,21 +89,10 @@ impl TryFrom<String> for Environment {
         match value.to_lowercase().as_str() {
             "local" => Ok(Self::Local),
             "production" => Ok(Self::Production),
-            other => Err(format!("{} is not a supported environment; use `local` or `production`", other))
+            other => Err(format!(
+                "{} is not a supported environment; use `local` or `production`",
+                other
+            )),
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_connection_string() {
-        let config = get_configuration().unwrap();
-        assert_eq!(
-            config.database.connection_string(),
-            "postgres://postgres:password@127.0.0.1:5432/newsletter"
-        )
     }
 }
